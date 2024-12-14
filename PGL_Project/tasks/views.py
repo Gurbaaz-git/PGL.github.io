a43c4a259bd9256215755aa817a5e7d8bc9d6eff
Gurbaaz <gurbaaz339@gmail.com>
2024-12-14 15:09:45 -0500

    First commit

---
+++ PGL_Project/tasks/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django import forms
+
+tasks = [""]
+class NewTaskForm(forms.Form):
+    task = forms.DecimalField(label="Side", min_value=0.000000000000000001)
+
+# Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+def square(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.pop(0)
+            tasks.append(task * task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form,
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm,
+        "tasks": tasks
+    })
